fa8f4c5e87ea49e41bc161d82798ee9f7fbb10e4
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-08-07 16:01:21 -0600

    Get books an authors to only be displayed once

--- script.js
+++ script.js
@@ -6,32 +6,14 @@ const bookInfo = {}
 let allBooks = []
 
 addBtn.addEventListener('click', function() {
-    let bookTitle = title.value
-    let bookAuthor = author.value
-    bookInfo.title = bookTitle
-    bookInfo.author = bookAuthor
-    allBooks.push(bookInfo)
-    for (let i = 0; i < allBooks.length; i++) {
+        let bookTitle = title.value
+        let bookAuthor = author.value
+        bookInfo.title = bookTitle
+        bookInfo.author = bookAuthor
+        allBooks.push(bookInfo)
+        for (let i = 0; i < allBooks.length; i++) {
         displayInput.innerHTML += `<li> ${ allBooks[i].title } by ${allBooks[i].author}</li>`
-                console.log(allBooks)
-
+        displayInput.innerHTML += `</span><button>Remove</button><span>`     
+                break
         }
-        console.log(bookInfo)
 })
-
-;//create an empty object
-
-// var info = {};//create an empty object
-// info.firstName = document.getElementById('firstName').value;
-// info.lastName = document.getElementById('lastName').value;
-// allInfo.push(info);//you had to initialize the array before
-
-
-// let bookTitle = title.value
-//     let bookAuthor = author.value
-//     booksTitle.push(bookTitle)
-//     booksAuthor.push(bookAuthor)
-//     for (let i = 0; i < booksTitle.length; i++) {
-//         displayInput.innerHTML += `<li> ${ booksTitle[i]} </li>`
-
-//     }

3b09a068343bc69600ae8a0475532e053f586b70
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-08-08 11:59:57 -0600

    Create function to display books in list, create function that removes book from list and function for local storage

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-let title = document.getElementById('title')
-let author = document.getElementById('author')
-let addBtn = document.getElementById('add-btn')
-let displayInput = document.getElementById('display-input')
-const bookInfo = {}
-let allBooks = []
-const booksFromLocalStorage = JSON.parse( localStorage.getItem("allBooks") )
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const addButton = document.getElementById('addButton');
+const bookList = document.getElementById('bookList');
 
-if (booksFromLocalStorage) {
-    allBooks = booksFromLocalStorage
+let books = JSON.parse(localStorage.getItem('books')) || [];
+
+addButton.addEventListener('click', () => {
+        const title = titleInput.value;
+        const author = authorInput.value;
+        if (title && author) {
+                books.push({ title, author });
+                saveBooks();
+                displayBooks();
+                titleInput.value = '';
+                authorInput.value = '';
+        } else {
+                alert('Please enter both title and author.');
         }
+});
 
-addBtn.addEventListener('click', function() {
-        let bookTitle = title.value
-        let bookAuthor = author.value
-        bookInfo.title = bookTitle
-        bookInfo.author = bookAuthor
-        allBooks.push(bookInfo)
-        for (let i = 0; i < allBooks.length; i++) {
-        displayInput.innerHTML += `<li> ${ allBooks[i].title } by ${allBooks[i].author}</li>`
-        displayInput.innerHTML += `</span><button id"removeBtn">Remove</button><span>`     
-                break
-        }
+function saveBooks() {
+        localStorage.setItem('books', JSON.stringify(books));
+}
 
-  localStorage.setItem("allBooks", JSON.stringify(allBooks) )
-})
+function displayBooks() {
+        bookList.innerHTML = '';
+        books.forEach((book, index) => {
+                const bookItem = document.createElement('div');
+                bookItem.classList.add('book-item');
+
+                const bookTitle = document.createElement('div');
+                bookTitle.classList.add('book-title');
+                bookTitle.textContent = `${book.title} by ${book.author}`;
+                bookItem.appendChild(bookTitle);
+
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'Remove';
+                removeButton.classList.add('remove-button');
+                removeButton.addEventListener('click', () => removeBook(index));
+                bookItem.appendChild(removeButton);
+        bookList.appendChild(bookItem);
+        });
+}
+
+function removeBook(index) {
+        books.splice(index, 1);
+        saveBooks();
+        displayBooks();
+}
+
+displayBooks();
+

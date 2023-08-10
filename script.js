471ae198eacbd0763ad56990a18445adc0f3292c
Christian Nualart <nualart@Christians-Mac-mini.local>
2023-08-10 14:09:49 -0600

    linter issues

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-if (typeof document !== 'undefined') {
-  const bookList = document.getElementById('bookList');
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-}
 
-
-function saveBooks() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function createBookItem(book, index) {
-  const bookItem = `
-    <div class="book-item">
-      <div class="book-title">${book.title} by ${book.author}</div>
-      <button class="remove-button" data-index="${index}">Remove</button>
-    </div>
-  `;
-
-  return bookItem;
-}
-
-function updateBookListDisplay() {
-  bookList.innerHTML = '';
-  books.forEach((book, index) => {
-    const bookItem = createBookItem(book, index);
-    bookList.insertAdjacentHTML('beforeend', bookItem);
-  });
-}
-
-function removeBook(index) {
-  books.splice(index, 1);
-  saveBooks();
-  updateBookListDisplay();
-}
 
 if (typeof document !== 'undefined') {
   const titleInput = document.getElementById('titleInput');
   const authorInput = document.getElementById('authorInput');
   const addButton = document.getElementById('addButton');
-  //const bookList = document.getElementById('bookList');
+  const bookList = document.getElementById('bookList');
 
-  //const books = JSON.parse(localStorage.getItem('books')) || [];
+  const books = JSON.parse(localStorage.getItem('books')) || [];
+
+  function saveBooks() {
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  
+  function createBookItem(book, index) {
+    const bookItem = `
+      <div class="book-item">
+        <div class="book-title">${book.title} by ${book.author}</div>
+        <button class="remove-button" data-index="${index}">Remove</button>
+      </div>
+    `;
+  
+    return bookItem;
+  }
+  
+  function updateBookListDisplay() {
+    bookList.innerHTML = '';
+    books.forEach((book, index) => {
+      const bookItem = createBookItem(book, index);
+      bookList.insertAdjacentHTML('beforeend', bookItem);
+    });
+  }
+  
+  function removeBook(index) {
+    books.splice(index, 1);
+    saveBooks();
+    updateBookListDisplay();
 
   addButton.addEventListener('click', () => {
     const title = titleInput.value;
